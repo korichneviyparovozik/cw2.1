6f9d77e7b7be8f361c06881576eb2ae156f67715
Миша <vorono594@gmail.com>
2024-01-27 10:40:48 +0100

    t4uih4f

---
+++ main.py
@@ -0,0 +1,24 @@
+class Human:
+    def __init__(self, name = "Human"):
+        self.name = name
+class Auto:
+    def __init__(self, brand):
+        self.brand = brand
+        self.passengers = []
+    def add_passenger(self, human):
+        self.passengers.append(human)
+    def print_passengers_names(self):
+        if self.passengers != []:
+            print(f"Names of {self.brand} passengers: ")
+            for passenger in self.passengers:
+                print(passenger.name)
+        else:
+            print(f"There is no passengers in {self.brand}")
+nick = Human("Nick")
+kate = Human("Kate")
+car = Auto("Volvo")
+car.add_passenger(nick)
+car.print_passengers_names()
+car.add_passenger(kate)
+car.print_passengers_names()
+
